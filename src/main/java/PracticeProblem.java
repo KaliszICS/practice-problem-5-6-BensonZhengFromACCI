1cb42dca50e8840cc4b662cbcc035bbae915b5b2
BensonZhengFromACCI <343804126@tdsb.ca>
2026-05-24 00:18:43 +0000

    what the

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,45 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
 	}
 
+	public static int getAge(String[] names, int[] ages, String name) {
+		int age = 0;
+		for (int start = 0; start < names.length; start++) {
+			if (name.equals(names[start])) {
+			return ages[start];
+			}
+		}
+	return 0;
+	}
+
+	public static int[] createLengthArray(String[] input) {
+		int[] numArr = new int[input.length];
+		for (int start = 0; start < input.length; start++) { 
+			numArr[start] = input[start].length();
+		}
+		return numArr;
+	}
+
+	public static void swapPeople(String[] names, int[] ages, String name, String name2) {
+		int first = -1;
+		int second = -1;
+
+		for (int start = 0; start < names.length; start++) {
+			if (first == -1 && names[start].equals(name)) {
+				first = start;
+			}
+			else if (second == -1 && names[start].equals(name2)) {
+				second = start;
+			}
+		}
+
+		String tempName = names[first];
+		names[first] = names[second];
+		names[second] = tempName;
+
+		int tempAge = ages[first];
+		ages[first] = ages[second];
+		ages[second] = tempAge;
+	}
 }
